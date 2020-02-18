03ef2e376f215e991351d7376cf5624394f8ae85
RasputinZA <47171762+RasputinZA@users.noreply.github.com>
2020-02-19 00:04:38 +0200

    Update App.java

--- adp3-Assignment1/src/main/java/za/ac/cput/App.java
+++ adp3-Assignment1/src/main/java/za/ac/cput/App.java
@@ -11,12 +11,17 @@ public class App
     {
         int num1, num2;
         int sum;
+        String name;
 
         System.out.println( "===========================================");
         System.out.println( "        S U M  C A L C U L A T O R         ");
         System.out.println( "===========================================");
 
         //User input
+        System.out.print("Name? ")
+        name = console.nextLine();
+        System.out.ptintln("Hello " + name + "!")
+            
         System.out.print( "Please enter a value for num1: " );
         num1 = console.nextInt();
 
@@ -27,6 +32,6 @@ public class App
         sum = num1 + num2;
 
         //output
-        System.out.println("Sum is: " + sum);
+        System.out.println("Sum is: " + sum + ". " + "Have a nice day " + name);
     }
 }
