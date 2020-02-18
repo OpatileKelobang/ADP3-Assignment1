f028e47aa5241f03c003933fe98b95377c9da110
Michael Bezuidenhout <61207961+217037240@users.noreply.github.com>
2020-02-18 23:15:38 +0200

    Add files via upload

---
+++ adp3-Assignment1/src/main/java/za/ac/cput/App.java
@@ -0,0 +1,32 @@
+package za.ac.cput;
+
+import java.util.Scanner;
+
+public class App
+{
+
+    static Scanner console = new Scanner(System.in);
+    public static void main( String[] args )
+
+    {
+        int num1, num2;
+        int sum;
+
+        System.out.println( "===========================================");
+        System.out.println( "        S U M  C A L C U L A T O R         ");
+        System.out.println( "===========================================");
+
+        //User input
+        System.out.print( "Please enter a value for num1: " );
+        num1 = console.nextInt();
+
+        System.out.print( "Please enter a value for num2: " );
+        num2 = console.nextInt();
+
+        //processing
+        sum = num1 + num2;
+
+        //output
+        System.out.println("Sum is: " + sum);
+    }
+}
